61820f0330691987493d5230f3d5c66829ed9450
Your Name <1367782488@qq.com>
2019-08-09 23:14:29 +0800

    果恋2--2019年8月

--- config/index.js
+++ config/index.js
@@ -34,7 +34,7 @@ module.exports = {
     index: path.resolve(__dirname, `../dist/${fileExt.platform}/index.html`),
     assetsRoot: path.resolve(__dirname, `../dist/${fileExt.platform}`),
     assetsSubDirectory: '',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
